c37693f0b1d3343ac7e95f7dbe6b7536da3cfd1c
Krzysztof Chadynka <krzysztof.chadynka@tivix.con>
2022-10-13 10:30:55 +0200

    Test commit

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from data_analyze.DataImporter import DataImporter
 data_importer = DataImporter()
 data_importer.create_sample_dataframe()
 print(data_importer.get_sample_dataframe())
+print('test')
